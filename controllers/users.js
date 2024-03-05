51575dd874ab0365452793c1f4a094877196a24f
atahantutar <atahantutar69@gmail.com>
2024-03-05 23:02:01 +0300

    Add user controller with login and register functions

---
+++ controllers/users.js
@@ -0,0 +1,8 @@
+import postgresClient from "../config/db.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+export const login = async (req, res) => {};
+export const register = async (req, res) => {};
+
+export default { register, login };
