2f0c7b57bdaf8f9667066cb99d1e1444bf932e87
atahantutar <atahantutar69@gmail.com>
2024-03-07 22:14:06 +0300

    Add login functionality to users controller

--- controllers/users.js
+++ controllers/users.js
@@ -5,7 +5,43 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const login = async (req, res) => {};
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const userCheck = await postgresClient.query(
+      "SELECT * FROM users WHERE email=$1 ",
+      [email]
+    );
+    if (userCheck.rowCount == 0) {
+      return res
+        .status(400)
+        .json({ message: "The email address is already registered." });
+    }
+
+    const comparePassword = await bcrypt.compare(
+      password,
+      (
+        await userCheck
+      ).rows[0].password
+    );
+    if (!comparePassword) {
+      return res
+        .status(400)
+        .json({ message: "Your password or email address is incorrect." });
+    }
+    const token = jwt.sign(
+      { id: (await userCheck).rows[0].id },
+      process.env.SECRET_TOKEN,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    res.header("Authorization", token).json({ accessToken: token });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 export const register = async (req, res) => {
   try {
     const { name, surname, email, password, address } = req.body;
