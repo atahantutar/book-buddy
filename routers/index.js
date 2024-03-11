4e0c7a02ae6cb172464601a7a3275a8990666e85
atahantutar <atahantutar69@gmail.com>
2024-03-11 15:45:04 +0300

    Add bookRouter to index.js

--- routers/index.js
+++ routers/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import userRouter from "./userRouter.js";
+import bookRouter from "./bookRouter.js";
 
 const router = express.Router();
 
 router.use(userRouter);
+router.use(bookRouter);
 
 export default router;
