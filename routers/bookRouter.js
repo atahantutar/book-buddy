a48ee2180fb8b89cea2900718b45d6db54e4ba76
atahantutar <atahantutar69@gmail.com>
2024-03-12 15:25:09 +0300

    Refactor bookRouter.js to use named imports for book controllers

--- routers/bookRouter.js
+++ routers/bookRouter.js
@@ -1,12 +1,18 @@
 import express from "express";
 import auth from "../middleware/auth.js";
-import adBook from "../controllers/books.js";
+import {
+  addBook,
+  deleteBook,
+  getAllBooks,
+  getUserBooks,
+} from "../controllers/books.js";
 
 const router = express.Router();
 
-router.get("/getbooks");
-router.post("/addbook", auth, adBook);
+router.get("/getbooks", getAllBooks);
+router.get("/getuserbooks", auth, getUserBooks);
+router.post("/addbook", auth, addBook);
 router.post("/updatebook", auth);
-router.delete("/books/:id", auth);
+router.delete("/books/:bookId", auth, deleteBook);
 
 export default router;
