ad04dd96be2c047e38813bc3e2d8dc9f74118af4
atahantutar <atahantutar69@gmail.com>
2024-03-12 15:25:02 +0300

    Add getAllBooks, getUserBooks, and deleteBook functions

--- controllers/books.js
+++ controllers/books.js
@@ -1,6 +1,30 @@
 import postgresClient from "../config/db.js";
 
-const addBook = async (req, res) => {
+export const getAllBooks = async (req, res) => {
+  try {
+    const getBooks = postgresClient.query("SELECT * FROM Books ");
+    res.status(200).json({ Books: (await getBooks).rows });
+  } catch (error) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
+};
+export const getUserBooks = async (req, res) => {
+  try {
+    const ownerId = req.userId;
+    const getBooks = postgresClient.query(
+      "SELECT * FROM Books WHERE ownerid=$1",
+      [ownerId]
+    );
+    res.status(200).json({ Books: (await getBooks).rows });
+  } catch (error) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
+};
+export const addBook = async (req, res) => {
   console.log(req.userId);
   try {
     const {
@@ -38,5 +62,27 @@ const addBook = async (req, res) => {
     res.status(401).json({ message: error.message });
   }
 };
+export const deleteBook = async (req, res) => {
+  try {
+    const bookID = req.params.bookId;
+    const ownerId = req.userId;
+    const checkBook = postgresClient.query(
+      "SELECT * FROM books WHERE bookid=$1 AND ownerid=$2",
+      [bookID, ownerId]
+    );
+    if ((await checkBook).rowCount == 0) {
+      res.status(404).json({ message: "Book not found." });
+    } else {
+      const deletedBook = postgresClient.query(
+        "DELETE FROM books WHERE bookid=$1 RETURNING *",
+        [bookID]
+      );
 
-export default addBook;
+      res.status(200).json({
+        deletedBook: (await deletedBook).rows,
+      });
+    }
+  } catch (error) {
+    res.status(401).json({ message: error.message });
+  }
+};
