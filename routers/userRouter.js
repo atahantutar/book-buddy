b204130f8151732db6ff6aa7ae803351cce7e3d5
atahantutar <atahantutar69@gmail.com>
2024-03-12 15:25:16 +0300

    Change HTTP methods for user registration and login routes

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,7 +3,7 @@ import { register, login } from "../controllers/users.js";
 
 const router = express.Router();
 
-router.get("/register", register);
-router.get("/login", login);
+router.post("/register", register);
+router.post("/login", login);
 
 export default router;
