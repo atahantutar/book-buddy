d059b8eb42db32abfc6a06362be75cdc729c7605
atahantutar <atahantutar69@gmail.com>
2024-03-23 23:09:10 +0300

    Add swapOffer, swapOffers, and swapRequests endpoints

--- controllers/books.js
+++ controllers/books.js
@@ -86,3 +86,61 @@ export const deleteBook = async (req, res) => {
     res.status(401).json({ message: error.message });
   }
 };
+export const swapOffer = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const bookId = req.body.bookId;
+    const ownerId = req.body.ownerId;
+    const date = new Date().toISOString();
+
+    const queryOffers =
+      " UPDATE users SET swap_offers = swap_offers || ARRAY[ARRAY[$1, $2,$3,$4 ]] WHERE id = $5";
+
+    const queryRequest =
+      " UPDATE users SET swap_requests = swap_requests || ARRAY[ARRAY[$1, $2,$3,$4 ]] WHERE id = $5";
+
+    await postgresClient.query(queryOffers, [
+      userId,
+      bookId,
+      "0",
+      date,
+      ownerId,
+    ]);
+    await postgresClient.query(queryRequest, [
+      ownerId,
+      bookId,
+      "0",
+      date,
+      userId,
+    ]);
+    res.status(200).json({ message: "Your swap offer has been delivered." });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+export const swapOffers = async (req, res) => {
+  try {
+    const userId = req.userId;
+
+    const query = "SELECT swap_offers FROM users WHERE id=$1";
+
+    const swapOffers = await postgresClient.query(query, [userId]);
+
+    res.status(200).json({ swapOffers: swapOffers.rows });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+export const swapRequests = async (req, res) => {
+  try {
+    const userId = req.userId;
+
+    const query = "SELECT swap_requests FROM users WHERE id=$1";
+
+    const swapOffers = await postgresClient.query(query, [userId]);
+
+    res.status(200).json({ swapOffers: swapOffers.rows });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
